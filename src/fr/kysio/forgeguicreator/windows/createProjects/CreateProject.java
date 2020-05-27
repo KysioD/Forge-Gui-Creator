0c0e10fc35f4ad5ba3874960613612d37b355d5f
Kysio <hugo.malaquin@gmail.com>
2020-05-27 13:52:49 +0200

    Finish create project windows

--- src/fr/kysio/forgeguicreator/windows/createProjects/CreateProject.java
+++ src/fr/kysio/forgeguicreator/windows/createProjects/CreateProject.java
@@ -1,15 +1,39 @@
 package fr.kysio.forgeguicreator.windows.createProjects;
 
 import fr.kysio.forgeguicreator.Main;
+import fr.kysio.forgeguicreator.utils.FilesManager;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class CreateProject {
+
+    @FXML
+    public TextField name;
+
+    @FXML
+    public TextField author;
+
+    @FXML
+    public MenuButton version;
 
     public void initialize(){
         Main.stage.setResizable(false);
     }
 
     public void createProject(){
+        if(!name.getText().isEmpty() && !author.getText().isEmpty()){
+            FilesManager.createProject(name.getText(), author.getText(), "1.12.2");
 
+            try {
+                Main.changeWindow(FXMLLoader.load(getClass().getResource("../projects/projects.fxml")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            };
+        }
     }
 
 }
