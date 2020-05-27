4a298c4df84a08e9e029ff6630bbc976e3453473
Kysio <hugo.malaquin@gmail.com>
2020-05-27 14:26:30 +0200

    Add project selector

--- src/fr/kysio/forgeguicreator/windows/projects/Projects.java
+++ src/fr/kysio/forgeguicreator/windows/projects/Projects.java
@@ -1,11 +1,14 @@
 package fr.kysio.forgeguicreator.windows.projects;
 
 import fr.kysio.forgeguicreator.Main;
+import fr.kysio.forgeguicreator.Project;
+import fr.kysio.forgeguicreator.utils.FilesManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Projects {
 
@@ -18,13 +21,49 @@ public class Projects {
     @FXML
     public Label version;
 
+    public ArrayList<Project> projects = FilesManager.getprojects();
+    public int selectedProject;
+
     public void initialize(){
+        this.selectedProject = 0;
         Main.stage.setResizable(true);
+
+        Project project = projects.get(selectedProject);
+
+        name.setText(project.getName());
+        author.setText(project.getAuthor());
+        version.setText(project.getVersion());
+    }
+
+    public void nextProject(){
+        if(selectedProject < projects.size()-1){
+            selectedProject++;
+        }
+
+
+        Project project = projects.get(selectedProject);
+
+        name.setText(project.getName());
+        author.setText(project.getAuthor());
+        version.setText(project.getVersion());
+    }
+
+    public void precedentProject(){
+        if(selectedProject > 0){
+            selectedProject--;
+        }
+
+
+        Project project = projects.get(selectedProject);
+
+        name.setText(project.getName());
+        author.setText(project.getAuthor());
+        version.setText(project.getVersion());
     }
 
     public void createProject(){
         try {
-            Main.changeWindow(FXMLLoader.load(getClass().getResource("../createProject/CreateProject.fxml")));
+            Main.changeWindow(FXMLLoader.load(getClass().getResource("../createprojects/CreateProject.fxml")));
         } catch (IOException e) {
             e.printStackTrace();
         };
